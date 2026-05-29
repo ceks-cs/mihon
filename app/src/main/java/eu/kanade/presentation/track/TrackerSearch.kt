2617db8f86ca556bb1a8679d3783cd7d14bccfa2
Ali Efruz Yıldırır <efruzyildirir@gmail.com>
2026-05-29 19:37:28 +0300

    fix invalid import 'androidx.compose.ui.input.pointer.awaitPointerEventScope'

--- app/src/main/java/eu/kanade/presentation/track/TrackerSearch.kt
+++ app/src/main/java/eu/kanade/presentation/track/TrackerSearch.kt
@@ -55,7 +55,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.input.pointer.awaitPointerEventScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
